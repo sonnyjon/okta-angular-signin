3f5a493557be229935f315c855aa945429d8137d
sonnyjon <sonny.s.jon@gmail.com>
2021-06-10 09:59:22 -0700

    changed import of environment file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 import {
   OKTA_CONFIG,
